1c5290af89f21533e4197431239b1e9406481868
devaniljr <contatodevanil@gmail.com>
2022-06-25 17:13:17 -0400

    Create Users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
   root "home#index"
   
-  resources :events, :controller=>"home" do
+  resources :events, :controller => "home" do
     resources :registrations
   end
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :users
+  get "signup" => "users#new"
 end
